ecf57060fe0bc3d08f9e4545320fa9341c98c1ef
Luis Bosquez <Luis@rhughes-0004.redmond.corp.microsoft.com>
2012-07-02 11:04:25 -0700

    First Commit. Adding all original files.

---
+++ ShortcutTrigger/SetShortcut.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MayhemWpf.UserControls;
+
+namespace ShortcutTrigger
+{
+    /// <summary>
+    /// Interaction logic for SetShortcut.xaml
+    /// </summary>
+    public partial class SetShortcut : WpfConfiguration
+    {
+        public int selectedIndex = 0;
+
+        public override string Title
+        {
+            get
+            {
+                return "Set Shortcut";
+            }
+        }
+        public override void OnLoad()
+        {
+            base.OnLoad();
+        }
+        public SetShortcut()
+        {
+            CanSave = true;
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedIndex = ShortcutType.SelectedIndex;
+        }
+    }
+}
---
+++ ShortcutTrigger/Shortcut.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using MayhemCore;
+using System.Windows.Input;
+using System.Windows.Forms;
+using MayhemWpf.ModuleTypes;
+using MayhemWpf.UserControls;
+
+namespace ShortcutTrigger
+{
+    [DataContract]
+
+    [MayhemModule("Keyboard Shortcut", "Triggers a keyboard shortcut in the current context.")] 
+
+
+    public class Shortcut: ReactionBase, IWpfConfigurable 
+    {
+
+        [DataMember]
+
+        private string[] option = { "(^c)", "(^v)", "(^p)", "(%{TAB})", "(^+){ESC}", "(%{F4})" };
+
+        private static int selected = 0;
+
+        public override void Perform()
+        {
+            try
+            {
+                SendKeys.SendWait(option[selected]);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("" + e.Message);
+            }
+        }
+
+
+
+        public WpfConfiguration ConfigurationControl
+        {
+            get { return new SetShortcut(); }
+        }
+
+        public void OnSaved(WpfConfiguration configurationControl)
+        {
+            selected = ((SetShortcut)configurationControl).selectedIndex;
+        }
+        
+        public string GetConfigString()
+        {
+            return "This";
+        }
+    }
+}
